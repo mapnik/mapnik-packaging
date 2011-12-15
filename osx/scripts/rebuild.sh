23b7ef6c9d2b911b2605b411f3923b1f162ad358
Dane Springmeyer <dane@dbsgeo.com>
2011-12-14 20:14:54 -0800

    main build script

---
+++ osx/scripts/rebuild.sh
@@ -0,0 +1,11 @@
+source settings.sh
+rm -rf ${MAPNIK_INSTALL}
+scripts/build_mapnik.sh
+scripts/post_build_fix.sh
+scripts/copy_headers.sh
+install_name_tool -id ${MAPNIK_INSTALL}/lib/libmapnik.dylib ${MAPNIK_INSTALL}/lib/libmapnik.dylib
+rm installer/*dmg
+rm installer/pkg/*pkg
+packagemaker --doc installer/mapnik.pmdoc --out installer/pkg/Mapnik.pkg
+packagemaker --doc installer/uninstall.pmdoc --out installer/pkg/Uninstall-Mapnik.pkg
+hdiutil create "installer/mapnik_2.0.0.dmg" -volname "Mapnik 2.0.0" -fs HFS+ -srcfolder installer/pkg
