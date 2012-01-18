9e1769247e78b661d161b0359de027b3a6eb3a1e
Dane Springmeyer <dane@dbsgeo.com>
2012-01-17 20:18:29 -0800

    touchup bjam wrapper

--- windows/build_mapnik.bat
+++ windows/build_mapnik.bat
@@ -1,9 +1,10 @@
 @echo off
 
-set BOOST_INCLUDES=d:\boost\include\boost-1_47
-set BOOST_LIBS=d:\boost\lib
-set MAPNIK_DEPS_DIR=d:\mapnik_build\mapnik_deps_vc9
-set MAPNIK_SOURCE=d:\mapnik_build\mapnik
+set BOOST_INCLUDES=c:\dev2\boost-vc100\include\boost-1_48
+set BOOST_LIBS=c:\dev2\boost-vc100\lib
+set MAPNIK_DEPS_DIR=c:\dev2\
+set MAPNIK_SOURCE=c:\dev2\mapnik
 set PREFIX=c:\mapnik-2.0
+set PATH=c:\dev2\boost_1_48_0;%PATH%
 
-bjam toolset=msvc-9.0 --prefix=%PREFIX% -sBOOST_INCLUDES=%BOOST_INCLUDES% -sBOOST_LIBS=%BOOST_LIBS% -sMAPNIK_DEPS_DIR=%MAPNIK_DEPS_DIR% -sMAPNIK_SOURCE=%MAPNIK_SOURCE%
+bjam toolset=msvc -j2 --prefix=%PREFIX% -sBOOST_INCLUDES=%BOOST_INCLUDES% -sBOOST_LIBS=%BOOST_LIBS% -sMAPNIK_DEPS_DIR=%MAPNIK_DEPS_DIR% -sMAPNIK_SOURCE=%MAPNIK_SOURCE%
