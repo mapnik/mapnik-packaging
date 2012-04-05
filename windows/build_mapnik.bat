34be75b8fc44cf78a23ef68efc9d860aa706123b
Dane Springmeyer <dane@dbsgeo.com>
2012-04-05 14:36:10 -0700

    restore variables used in jam files

--- windows/build_mapnik.bat
+++ windows/build_mapnik.bat
@@ -1,11 +1,16 @@
 @echo off
 
 set ROOTDIR=c:\dev2
+@rem note - MAPNIK_DEPS_DIR and MAPNIK_SOURCE
+@rem are needed by bjam files
+@rem so do not rename or remove them
+set MAPNIK_DEPS_DIR=%ROOTDIR%
+set MAPNIK_SOURCE=%ROOTDIR%\mapnik
+
 set BOOST_VERSION=49
 set BOOST_PREFIX=boost-%BOOST_VERSION%-vc100
 set BOOST_INCLUDES=%ROOTDIR%\%BOOST_PREFIX%\include\boost-1_%BOOST_VERSION%
 set BOOST_LIBS=%ROOTDIR%\%BOOST_PREFIX%\lib
-set MAPNIK_SOURCE=%ROOTDIR%\mapnik
 set PREFIX=c:\mapnik-2.0
 set PATH=%ROOTDIR%\boost_1_%BOOST_VERSION%_0;%PATH%
 
