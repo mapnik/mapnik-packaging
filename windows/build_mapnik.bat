f59ab4eebc979a504c80388b7f29ff0960a8e5c9
Dane Springmeyer <dane@dbsgeo.com>
2012-04-17 18:57:30 -0700

    copy expat into place

--- windows/build_mapnik.bat
+++ windows/build_mapnik.bat
@@ -14,4 +14,7 @@ set BOOST_LIBS=%ROOTDIR%\%BOOST_PREFIX%\lib
 set PREFIX=c:\mapnik-2.0
 set PATH=%ROOTDIR%\boost_1_%BOOST_VERSION%_0;%PATH%
 
+@rem copy expat, needed by ogr plugin
+copy "C:\Program Files (x86)\Expat 2.1.0\Bin\libexpat.dll" %PREFIX%\lib\mapnik\input\
+
 bjam toolset=msvc -j2 --prefix=%PREFIX% -sBOOST_INCLUDES=%BOOST_INCLUDES% -sBOOST_LIBS=%BOOST_LIBS% -sMAPNIK_DEPS_DIR=%MAPNIK_DEPS_DIR% -sMAPNIK_SOURCE=%MAPNIK_SOURCE%
