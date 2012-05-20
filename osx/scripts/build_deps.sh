1df400b0c5d042fb57c91edee8ed967cbe255d05
Dane Springmeyer <dane@dbsgeo.com>
2012-05-20 08:35:20 -0700

    defer-strile-load with libtiff

--- osx/scripts/build_deps.sh
+++ osx/scripts/build_deps.sh
@@ -188,6 +188,7 @@ export CFLAGS="-DHAVE_APPLE_OPENGL_FRAMEWORK $CFLAGS"
 --enable-static --disable-shared \
 --disable-dependency-tracking \
 --disable-cxx \
+--enable-defer-strile-load \
 --with-jpeg-include-dir=${BUILD}/include --with-jpeg-lib-dir=${BUILD}/lib \
 --with-zlib-include-dir=${BUILD}/include --with-zlib-lib-dir=${BUILD}/lib \
 --disable-lzma --disable-jbig --disable-mdi \
