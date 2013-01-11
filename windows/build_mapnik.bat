f70a99675cfcb4c4aae43d51f6ce7b1e8d9f49ec
Dane Springmeyer <dane@dbsgeo.com>
2013-01-10 19:07:48 -0800

    add support for sparsehash headers

--- windows/build_mapnik.bat
+++ windows/build_mapnik.bat
@@ -17,6 +17,9 @@ set PATH=%ROOTDIR%\boost_1_%BOOST_VERSION%_0;%PATH%
 
 bjam toolset=msvc -j2 --prefix=%PREFIX% -sBOOST_INCLUDES=%BOOST_INCLUDES% -sBOOST_LIBS=%BOOST_LIBS% -sMAPNIK_DEPS_DIR=%MAPNIK_DEPS_DIR% -sMAPNIK_SOURCE=%MAPNIK_SOURCE%
 
+@rem copy sparsehash - TODO add this copy to the Jamroot
+xcopy /i /s %MAPNIK_SOURCE%\deps\mapnik\sparsehash %PREFIX%\include\mapnik\sparsehash
+
 @rem copy all libs of dependencies
 @rem expat, needed by ogr plugin
 copy "C:\Program Files (x86)\Expat 2.1.0\Bin\libexpat.dll" %PREFIX%\lib\
