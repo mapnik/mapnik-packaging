acb068fa86bca3751ca3e71ee2b5477935d1d683
Dane Springmeyer <dane@dbsgeo.com>
2013-03-11 08:17:09 -0700

    update to latest mapnik and enable build of python bindings

--- windows/build_mapnik.bat
+++ windows/build_mapnik.bat
@@ -15,8 +15,6 @@ set BOOST_LIBS=%ROOTDIR%\%BOOST_PREFIX%\lib
 set PREFIX=c:\mapnik-2.0
 set PATH=%ROOTDIR%\boost_1_%BOOST_VERSION%_0;%PATH%
 
-bjam toolset=msvc -j2 --prefix=%PREFIX% -sBOOST_INCLUDES=%BOOST_INCLUDES% -sBOOST_LIBS=%BOOST_LIBS% -sMAPNIK_DEPS_DIR=%MAPNIK_DEPS_DIR% -sMAPNIK_SOURCE=%MAPNIK_SOURCE%
-
 @rem copy sparsehash - TODO add this copy to the Jamroot
 xcopy /i /s %MAPNIK_SOURCE%\deps\mapnik\sparsehash %PREFIX%\include\mapnik\sparsehash /Y
 xcopy /i /s %MAPNIK_SOURCE%\deps\agg\include %PREFIX%\include\mapnik\agg /Y
@@ -31,4 +29,9 @@ copy %ROOTDIR%\icu\bin\icudt48.dll %PREFIX%\lib\
 copy %ROOTDIR%\icu\bin\icuin48.dll %PREFIX%\lib\
 copy %ROOTDIR%\libxml2\win32\bin.msvc\libxml2.dll %PREFIX%\lib\
 copy %ROOTDIR%\proj\src\proj.dll %PREFIX%\lib\
+
+bjam toolset=msvc -j2 --python=true --prefix=%PREFIX% -sBOOST_INCLUDES=%BOOST_INCLUDES% -sBOOST_LIBS=%BOOST_LIBS% -sMAPNIK_DEPS_DIR=%MAPNIK_DEPS_DIR% -sMAPNIK_SOURCE=%MAPNIK_SOURCE%
+
+@rem python - paths.py, printing.py, __init__.py, boost python dll
 echo Started at %STARTTIME%, finished at %TIME%
+
