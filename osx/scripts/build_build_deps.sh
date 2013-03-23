2f4a811ae0dcc6f3c337cb47fc9be089cba5471a
Dane Springmeyer <dane@dbsgeo.com>
2013-03-23 16:12:46 -0700

    separate out build depedencies

---
+++ osx/scripts/build_build_deps.sh
@@ -0,0 +1,16 @@
+# xz
+echo '*building xz*'
+tar xf xz-5.0.3.tar.bz2
+cd xz-5.0.3
+./configure
+make -j$JOBS
+make install
+cd ${PACKAGES}
+
+# nose
+#tar xf nose-1.2.1.tar.gz
+#cd nose-1.2.1
+#sudo python3.3 setup.py install
+# sudo will hang script
+#sudo python setup.py install
+#cd ${PACKAGES}
