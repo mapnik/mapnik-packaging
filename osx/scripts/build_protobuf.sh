4875553abb2d100dcb39611d37bc37e812fba8b7
Dane Springmeyer <dane@dbsgeo.com>
2013-04-02 14:57:18 -0700

    get cross compile working of protobuf

--- osx/scripts/build_protobuf.sh
+++ osx/scripts/build_protobuf.sh
@@ -1,9 +1,15 @@
 cd ${PACKAGES}
 echo '*building protobuf C++*'
-rm -rf cd protobuf-${PROTOBUF_VERSION}
+rm -rf protobuf-${PROTOBUF_VERSION}-${ARCH_NAME}
 tar xf protobuf-${PROTOBUF_VERSION}.tar.bz2
-cd protobuf-${PROTOBUF_VERSION}
-./configure --prefix=${BUILD} ${HOST_ARG} \
+mv protobuf-${PROTOBUF_VERSION} protobuf-${PROTOBUF_VERSION}-${ARCH_NAME}
+cd protobuf-${PROTOBUF_VERSION}-${ARCH_NAME}
+if [ $BOOST_ARCH = "arm" ]; then
+    export CROSS_FLAGS="--with-protoc=$(pwd)/../protobuf-${PROTOBUF_VERSION}-i386/src/protoc"
+else
+    export CROSS_FLAGS=""
+fi
+./configure --prefix=${BUILD} ${HOST_ARG} ${CROSS_FLAGS} \
 --enable-static --disable-shared \
 --disable-debug --with-zlib \
 --disable-dependency-tracking
