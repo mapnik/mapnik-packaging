eb5bdcdaa9211caa1ca470a6f5e7f43ab78d7e97
Dane Springmeyer <dane@dbsgeo.com>
2013-06-19 12:39:15 -0700

    move pycairo to python build step

--- osx/scripts/build_deps_optional.sh
+++ osx/scripts/build_deps_optional.sh
@@ -295,32 +295,3 @@ export png_LIBS="-I${BUILD}/lib -lpng"
 make -j${JOBS}
 make install
 cd ${PACKAGES}
-
-# py2cairo
-echo '*building py2cairo*'
-rm -rf py2cairo-${PY2CAIRO_VERSION}
-tar xf py2cairo-${PY2CAIRO_VERSION}.tar.bz2
-cd py2cairo-${PY2CAIRO_VERSION}
-# apply patch
-patch wscript < ${PATCHES}/py2cairo-static.diff
-for i in {"2.6","2.7"}
-do
-    PYTHON=python$i ./waf configure --prefix=${BUILD} --nopyc --nopyo
-    PYTHON=python$i ./waf install
-done
-cd ${PACKAGES}
-
-# py3cairo
-echo '*building py3cairo*'
-rm -rf pycairo-${PY3CAIRO_VERSION}
-tar xf pycairo-${PY3CAIRO_VERSION}.tar.bz2
-cd pycairo-${PY3CAIRO_VERSION}
-# apply patch
-patch wscript < ${PATCHES}/py3cairo-static.diff
-export PATH=/Library/Frameworks/Python.framework/Versions/3.3/bin/:$PATH
-for i in {"3.3",}
-do
-    PYTHON=python$i ./waf configure --prefix=${BUILD} --nopyc --nopyo
-    PYTHON=python$i ./waf install
-done
-cd ${PACKAGES}
--- osx/scripts/build_python_versions.sh
+++ osx/scripts/build_python_versions.sh
@@ -38,3 +38,33 @@ mv stage/lib/libboost_python3.a stage/lib/libboost_python-3.3.a
 cp stage/lib/libboost_python-3.3.a ${BUILD}/lib/libboost_python-3.3.a
 
 cd ${PACKAGES}
+
+# py2cairo
+echo '*building py2cairo*'
+rm -rf py2cairo-${PY2CAIRO_VERSION}
+tar xf py2cairo-${PY2CAIRO_VERSION}.tar.bz2
+cd py2cairo-${PY2CAIRO_VERSION}
+# apply patch
+patch wscript < ${PATCHES}/py2cairo-static.diff
+for i in {"2.6","2.7"}
+do
+    PYTHON=python$i ./waf configure --prefix=${BUILD} --nopyc --nopyo
+    PYTHON=python$i ./waf install
+done
+cd ${PACKAGES}
+
+# py3cairo
+echo '*building py3cairo*'
+rm -rf pycairo-${PY3CAIRO_VERSION}
+tar xf pycairo-${PY3CAIRO_VERSION}.tar.bz2
+cd pycairo-${PY3CAIRO_VERSION}
+# apply patch
+patch wscript < ${PATCHES}/py3cairo-static.diff
+export PATH=/Library/Frameworks/Python.framework/Versions/3.3/bin/:$PATH
+for i in {"3.3",}
+do
+    PYTHON=python$i ./waf configure --prefix=${BUILD} --nopyc --nopyo
+    PYTHON=python$i ./waf install
+done
+cd ${PACKAGES}
+
