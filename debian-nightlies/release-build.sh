eb2da8ae77fff4ecf1c3e7c5576fbc7639d4b7e4
Dane Springmeyer <dane@dbsgeo.com>
2013-06-25 08:51:37 -0700

    Update release-build.sh

--- debian-nightlies/release-build.sh
+++ debian-nightlies/release-build.sh
@@ -10,12 +10,15 @@ DEBFULLNAME="Dane Springmeyer (Mapnik Releases)"
 DEBEMAIL="dane.springmeyer@gmail.com"
 DEST="mapnik" # the launchpad account name
 DISTS="saucy raring quantal precise lucid"
+#DIST="precise"
 PACKAGE="mapnik"
-BUILD_VERSION="2.1.0"
+BUILD_VERSION="2.2.0"
 DATE_REPR=$(date -R)
 SOURCE="${PACKAGE}_${BUILD_VERSION}"
 ORIG_TAR="${SOURCE}.orig.tar.bz2"
 PPA="ppa:${DEST}/v${BUILD_VERSION}"
+UBUNTU_VERSION="1"
+DISTRO_VERSION="1"
 
 mkdir deb-packaging
 cd deb-packaging
@@ -26,12 +29,12 @@ mv mapnik-v${BUILD_VERSION} "${SOURCE}"
 tar cjf "${ORIG_TAR}" "${SOURCE}/"
 
 git clone https://github.com/mapnik/mapnik-packaging
-cp -r "mapnik-packaging/debian-nightlies/master/debian" "${SOURCE}/"
+cp -r "mapnik-packaging/debian-nightlies/2.2.x/debian" "${SOURCE}/"
 
 CHANGELOG_NOTE="v${BUILD_VERSION} release (https://github.com/mapnik/mapnik/wiki/Release${BUILD_VERSION})"
 
 for DIST in ${DISTS}; do
-  DIST_VERSION="${BUILD_VERSION}-ubuntu1~${DIST}1";
+  DIST_VERSION="${BUILD_VERSION}-ubuntu${UBUNTU_VERSION}~${DIST}${DISTRO_VERSION}";
   echo "${PACKAGE} (${DIST_VERSION}) ${DIST}; urgency=medium" > "${SOURCE}/debian/changelog"
   echo  >> "${SOURCE}/debian/changelog"
   echo "  * ${CHANGELOG_NOTE}" >> "${SOURCE}/debian/changelog"
