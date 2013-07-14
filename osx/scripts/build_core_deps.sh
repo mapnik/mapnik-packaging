009c3cc96d14ba3cac5ed3aea3d9e20db593efc2
Dane Springmeyer <dane@dbsgeo.com>
2013-07-14 13:49:48 -0400

    fix libpng compile against custom zlib.h

--- osx/scripts/build_core_deps.sh
+++ osx/scripts/build_core_deps.sh
@@ -74,6 +74,8 @@ echo '*building libpng*'
 rm -rf libpng-${LIBPNG_VERSION}
 tar xf libpng-${LIBPNG_VERSION}.tar.gz
 cd libpng-${LIBPNG_VERSION}
+# NOTE: C_INCLUDE_PATH is needed for png the gcc -E usage which does not
+# seem to respect CFLAGS and will fail to find zlib.h
 ./configure --prefix=${BUILD} --enable-static --disable-shared ${HOST_ARG} \
   --disable-dependency-tracking \
   --with-zlib-prefix=${BUILD}
