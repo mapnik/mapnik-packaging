a72906cea12d1d929c38c48e3607928f037908fa
Dane Springmeyer <dane@dbsgeo.com>
2013-08-20 20:11:58 -0400

    bump node version to v0.10.16

--- osx/settings.sh
+++ osx/settings.sh
@@ -219,5 +219,14 @@ export PROTOBUF_VERSION="2.5.0"
 export PROTOBUF_C_VERSION="0.15"
 export XZ_VERSION="5.0.3"
 export NOSE_VERSION="1.2.1"
-export NODE_VERSION="0.10.15"
+export NODE_VERSION="0.10.16"
+
+function download {
+    if [ ! -f $1 ]; then
+        echo downloading $1
+        curl -s -S -f -O  ${S3_BASE}/$1
+    else
+        echo using cached $1
+    fi
+}
 
