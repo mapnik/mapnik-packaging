58d2fdaa24ae10ab9678499b4515856ed65a91be
Dane Springmeyer <dane@dbsgeo.com>
2013-09-09 14:00:29 -0700

    update node to v0.10.18

--- osx/settings.sh
+++ osx/settings.sh
@@ -230,7 +230,7 @@ export PROTOBUF_VERSION="2.5.0"
 export PROTOBUF_C_VERSION="0.15"
 export XZ_VERSION="5.0.3"
 export NOSE_VERSION="1.2.1"
-export NODE_VERSION="0.10.17"
+export NODE_VERSION="0.10.18"
 
 function download {
     if [ ! -f $1 ]; then
