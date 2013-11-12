a9b02319ed7b91595ef97d17391d1b3291341ba8
Dane Springmeyer <dane@dbsgeo.com>
2013-11-11 19:18:05 -0800

    build freetype without bzip2 compressed fonts and png compressed OpenType embedded bitmaps

--- osx/scripts/build_core_deps.sh
+++ osx/scripts/build_core_deps.sh
@@ -46,7 +46,9 @@ echo '*building freetype*'
 rm -rf freetype-${FREETYPE_VERSION}
 tar xf freetype-${FREETYPE_VERSION}.tar.bz2
 cd freetype-${FREETYPE_VERSION}
-./configure --prefix=${BUILD} --enable-static --disable-shared ${HOST_ARG}
+./configure --prefix=${BUILD} --enable-static --disable-shared ${HOST_ARG} \
+ --without-bzip2 \
+ --without-png
 make -j${JOBS}
 make install
 cd ${PACKAGES}
