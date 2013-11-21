9a1e03df0af1813b859c591429fc531edf54b8eb
Dane Springmeyer <dane@dbsgeo.com>
2013-11-20 20:28:23 -0800

    tweak rebuild script

--- osx/scripts/rebuild.sh
+++ osx/scripts/rebuild.sh
@@ -1,8 +1,10 @@
 source MacOSX.sh
 #./scripts/setup.sh
-#./scripts/download_deps.sh
+./scripts/download_deps.sh
 ./scripts/build_core_deps.sh
-#./scripts/build_deps_cairo.sh
+./scripts/build_deps_optional.sh
+./scripts/build_python_versions.sh
+./scripts/build_protobuf.sh
 
 # update mapnik
 #cd mapnik
@@ -14,20 +16,20 @@ rm -rf ${MAPNIK_BIN_SOURCE}
 cd scripts/
 
 # build mapnik
-./build_mapnik.sh
+#./build_mapnik.sh
 
 # make portable
-./post_build_fix.sh
+#./post_build_fix.sh
 
 # test mapnik
-./test_mapnik.sh
+#./test_mapnik.sh
 
 # ./package_minimal_binary_sdk.sh
 
 # manually edit mapnik-config if this is not a full SDK build
 
 # package dmg
-./package_dmg.sh
+#./package_dmg.sh
 
 # copy minimal headers of deps
 #./copy_headers.sh
