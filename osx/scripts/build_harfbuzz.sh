8f75f05638d212be570b8a1b71a38b4c59873c20
Dane Springmeyer <dane@dbsgeo.com>
2013-11-20 19:14:53 -0800

    harfbuzz build script

---
+++ osx/scripts/build_harfbuzz.sh
@@ -0,0 +1,26 @@
+set -e 
+
+mkdir -p ${PACKAGES}
+cd ${PACKAGES}
+
+# proj4
+echo '*building harfbuzz*'
+rm -rf harfbuzz-${HARFBUZZ_VERSION}
+tar xf harfbuzz-${HARFBUZZ_VERSION}.tar.bz2
+cd harfbuzz-${HARFBUZZ_VERSION}
+export OLD_LDFLAGS=${LDFLAGS}
+export LDFLAGS="${STDLIB_LDFLAGS} ${LDFLAGS}"
+./configure --prefix=${BUILD} ${HOST_ARG} \
+ --enable-static --disable-shared --disable-dependency-tracking \
+ --with-icu \
+ --with-cairo=no \
+ --with-glib=no \
+ --with-gobject=no \
+ --with-graphite2=no \
+ --with-freetype \
+ --with-uniscribe=no \
+ --with-coretext=no
+make -j${JOBS}
+make install
+export LDFLAGS=${OLD_LDFLAGS}
+cd ${PACKAGES}
