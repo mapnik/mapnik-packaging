75a9300dcffef66f4ea105f52c49bdf5cc9e1951
Dane Springmeyer <dane@dbsgeo.com>
2013-11-22 18:13:02 -0800

    cleanup node build

--- osx/scripts/build_node.sh
+++ osx/scripts/build_node.sh
@@ -7,16 +7,9 @@ echo '*building node*'
 rm -rf node-v${NODE_VERSION}
 tar xf node-v${NODE_VERSION}.tar.gz
 cd node-v${NODE_VERSION}
-cd tools/gyp
-# https://github.com/joyent/node/issues/3681
-curl -o issue_292.diff https://codereview.chromium.org/download/issue14887003_1_2.diff
-patch pylib/gyp/xcode_emulation.py issue_292.diff
-cd ../../
-export mac_deployment_target=10.7 # todo this should override 10.5 in standalone.gypi
 export OLD_LDFLAGS=${LDFLAGS}
-export OLD_CXXFLAGS=${CXXFLAGS}
 export CXXFLAGS="-mmacosx-version-min=10.7 ${CXXFLAGS}"
-export LDFLAGS="-mmacosx-version-min=10.7 ${STDLIB_LDFLAGS} ${LDFLAGS}"
+export LDFLAGS="${STDLIB_LDFLAGS} ${LDFLAGS}"
 ./configure --prefix=${BUILD} \
  --shared-zlib \
  --shared-zlib-includes=${BUILD}/include \
@@ -24,4 +17,3 @@ export LDFLAGS="-mmacosx-version-min=10.7 ${STDLIB_LDFLAGS} ${LDFLAGS}"
 make -j${JOBS}
 make install
 export LDFLAGS=${OLD_LDFLAGS}
-export CXXFLAGS=${OLD_CXXFLAGS}
