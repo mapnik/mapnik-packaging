7cf8cf0bfe8e476a71a2d4e066247f6d3665f5cb
Dane Springmeyer <dane@dbsgeo.com>
2013-12-11 12:49:45 -0800

    fix osm-pbf build

--- osx/scripts/build_osm-pbf.sh
+++ osx/scripts/build_osm-pbf.sh
@@ -4,15 +4,13 @@ set -e -u
 mkdir -p ${PACKAGES}
 cd ${PACKAGES}
 
-rm -rf v1.3.0
-curl -s -S -f -O https://codeload.github.com/scrosby/OSM-binary/tar.gz/v1.3.0
-tar xf v1.3.0
-cd OSM-binary-1.3.0/src
-patch -N < $PATCHES/osm-pbf.diff
+rm -rf OSM-binary
+git clone https://github.com/scrosby/OSM-binary.git
+cd OSM-binary/src
 if [ "${AR:-false}" != false ]; then
   make CXX=$CXX AR=$AR CXXFLAGS="${CXXFLAGS}"
 else
   make CXX=$CXX CXXFLAGS="${CXXFLAGS}"
 fi 
-make install DESTDIR=${BUILD}
+make install PREFIX=${BUILD}
 cd ${PACKAGES}
