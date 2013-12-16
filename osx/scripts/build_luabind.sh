3a0859981e7e0d35c202bff68434e065c80b9305
Dane Springmeyer <dane@dbsgeo.com>
2013-12-16 16:16:32 -0500

    switch back to forked luabind since current release fails to build on ubuntu

--- osx/scripts/build_luabind.sh
+++ osx/scripts/build_luabind.sh
@@ -7,6 +7,10 @@ cd ${PACKAGES}
 download luabind-${LUABIND_VERSION}.tar.gz
 
 echo '*building luabind*'
+
+# disabled because I failed to get bjam working on linux:
+#  https://gist.github.com/springmeyer/17d0a1a12d7d4bce74d4
+: '
 rm -rf luabind-${LUABIND_VERSION}
 tar xf luabind-${LUABIND_VERSION}.tar.gz
 cd luabind-${LUABIND_VERSION}
@@ -17,6 +21,7 @@ patch -N luabind/detail/format_signature.hpp ${PATCHES}/3044a9053ac50977684a75c4
 #wget https://gist.github.com/DennisOSRM/a246514bf7d01631dda8/raw/0e83503dbf862ebfb6ac063338a6d7bca793f94d/object_rep.diff
 patch -N luabind/detail/object_rep.hpp ${PATCHES}/object_rep.diff
 BOOST_ROOT=../boost_${BOOST_VERSION2}-${ARCH_NAME}
+# linux needs: sudo ln -s /usr/lib/x86_64-linux-gnu/liblua5.1.so /usr/lib/liblua.so
 ${BOOST_ROOT}/b2 \
   -d2 \
   --prefix=${BUILD} \
@@ -28,8 +33,8 @@ ${BOOST_ROOT}/b2 \
   cxxflags="${CXXFLAGS}" \
   -sBOOST_ROOT=${BOOST_ROOT} \
   stage install
+'
 
-: '
 rm -rf luabind
 git clone https://github.com/DennisOSRM/luabind.git
 cd luabind
@@ -48,6 +53,6 @@ cmake ../ -DCMAKE_INSTALL_PREFIX=${BUILD} \
 make -j${JOBS} VERBOSE=1
 make install
 export LINK_FLAGS=${OLD_LINK_FLAGS}
-'
+
 
 cd ${PACKAGES}
