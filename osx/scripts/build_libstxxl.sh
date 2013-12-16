ebf999fb48f8d72b730eb87ab0843c56e43942f2
Dane Springmeyer <dane@dbsgeo.com>
2013-12-16 13:48:31 -0500

    fix stxxl build against libstdc++

--- osx/scripts/build_libstxxl.sh
+++ osx/scripts/build_libstxxl.sh
@@ -5,6 +5,7 @@ mkdir -p ${PACKAGES}
 cd ${PACKAGES}
 
 echo '*building libsxxl*'
+rm -rf stxxl-${STXXL_VERSION}
 rm -rf stxxl-${STXXL_VERSION}-${ARCH_NAME}
 tar xf stxxl-${STXXL_VERSION}.tar.gz
 mv stxxl-${STXXL_VERSION} stxxl-${STXXL_VERSION}-${ARCH_NAME}
@@ -13,12 +14,18 @@ export OLD_LDFLAGS=${LDFLAGS}
 export LDFLAGS="${STDLIB_LDFLAGS} ${LDFLAGS}"
 mkdir build
 cd build
+if [ ${CXX11} = true ]; then
+    CXX_ENABLED="-DNO_CXX11=OFF"
+else
+    CXX_ENABLED="-DNO_CXX11=ON"
+fi
 cmake ../ -DCMAKE_INSTALL_PREFIX=${BUILD} \
   -DBoost_NO_SYSTEM_PATHS=ON \
   -DCMAKE_INCLUDE_PATH=${BUILD}/include \
   -DCMAKE_LIBRARY_PATH=${BUILD}/lib \
   -DBUILD_STATIC_LIBS=ON \
-  -DCMAKE_BUILD_TYPE=Release
+  -DCMAKE_BUILD_TYPE=Release \
+  ${CXX_ENABLED}
 make -j${JOBS} VERBOSE=1
 make install
 export LDFLAGS=${OLD_LDFLAGS}
