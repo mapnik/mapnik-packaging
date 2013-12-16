940890dbe84b6b8454afef7155bb6aa5ebd8e965
Dane Springmeyer <dane@dbsgeo.com>
2013-12-16 12:45:07 -0500

    add script to build Project-OSRM from develop branch

---
+++ osx/scripts/build_osrm.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -e -u
+
+mkdir -p ${PACKAGES}
+cd ${PACKAGES}
+
+echo '*building OSRM*'
+rm -rf Project-OSRM
+git clone https://github.com/DennisOSRM/Project-OSRM.git -b develop --depth 1
+cd Project-OSRM
+# NOTE: cmake ignores LDFLAGS but respects LINK_FLAGS in env
+if [ "${LINKFLAGS:-false}" = false ]; then
+  LINK_FLAGS=""
+fi
+export OLD_LINK_FLAGS=${LINK_FLAGS}
+export LINK_FLAGS="${STDLIB_LDFLAGS} ${LDFLAGS}"
+mkdir build
+cd build
+cmake ../ -DCMAKE_INSTALL_PREFIX=${BUILD} \
+  -DBoost_NO_SYSTEM_PATHS=ON \
+  -DCMAKE_INCLUDE_PATH=${BUILD}/include \
+  -DCMAKE_LIBRARY_PATH=${BUILD}/lib \
+  -DBUILD_STATIC_LIBS=ON \
+  -DCMAKE_BUILD_TYPE=Release
+make -j${JOBS} VERBOSE=1
+make install
+export LINK_FLAGS=${OLD_LINK_FLAGS}
+cd ${PACKAGES}
+
+#check_and_clear_libs
