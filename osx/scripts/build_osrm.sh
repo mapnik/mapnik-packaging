6457cb4efc93a7f8be653d4123f218e55c9181c5
Dane Springmeyer <dane@dbsgeo.com>
2013-12-17 15:03:15 -0500

    clear out osrm directory

--- osx/scripts/build_osrm.sh
+++ osx/scripts/build_osrm.sh
@@ -10,7 +10,8 @@ git clone https://github.com/DennisOSRM/Project-OSRM.git -b develop --depth 1
 cd Project-OSRM
 export OLD_LINK_FLAGS=${LINK_FLAGS}
 export LINK_FLAGS="${STDLIB_LDFLAGS} ${LINK_FLAGS}"
-mkdir build
+rm -rf build
+mkdir -p build
 cd build
 cmake ../ -DCMAKE_INSTALL_PREFIX=${BUILD} \
   -DBoost_NO_SYSTEM_PATHS=ON \
