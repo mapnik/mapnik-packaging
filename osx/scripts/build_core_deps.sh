3d7eb41c391a4e0673ebf18fabf0340ec73b733a
Dane Springmeyer <dane@dbsgeo.com>
2013-12-19 18:23:22 -0500

    require args to boost build script

--- osx/scripts/build_core_deps.sh
+++ osx/scripts/build_core_deps.sh
@@ -8,7 +8,13 @@ cd ${PACKAGES}
 ${ROOTDIR}/scripts/build_icu.sh
 
 # boost
-${ROOTDIR}/scripts/build_boost.sh
+BOOST_LIBRARIES="--with-thread --with-filesystem --disable-filesystem2 --with-system --with-regex"
+if [ ${BOOST_ARCH} != "arm" ]; then
+    BOOST_LIBRARIES="$BOOST_LIBRARIES --with-program_options"
+    # --with-chrono --with-iostreams --with-date_time --with-atomic --with-timer --with-program_options --with-test
+fi
+
+${ROOTDIR}/scripts/build_boost.sh $BOOST_LIBRARIES
 
 # bzip2
 ${ROOTDIR}/scripts/build_bzip2.sh
