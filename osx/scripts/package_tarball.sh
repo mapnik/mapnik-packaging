c3ae2dff23371beb568a54489a3d9ab6e4f28202
Dane Springmeyer <dane@dbsgeo.com>
2013-12-19 19:38:16 -0500

    osrm needs boost program options and iostreams

--- osx/scripts/package_tarball.sh
+++ osx/scripts/package_tarball.sh
@@ -12,12 +12,3 @@ ln -s ${MAPNIK_BIN_SOURCE} ${MAPNIK_DIST}/${MAPNIK_PACKAGE_PREFIX}
 tar cjfH ${MAPNIK_DIST}/mapnik-${FOUND_VERSION}.tar.bz2 ${MAPNIK_PACKAGE_PREFIX}/
 # cleanup symlink
 rm ${MAPNIK_DIST}/${MAPNIK_PACKAGE_PREFIX}
-
-# copy approach
-<<COMMENT
-mkdir -p ${MAPNIK_PACKAGE_PREFIX}
-rm -rf ./${MAPNIK_PACKAGE_PREFIX}/*
-cp -R ${MAPNIK_BIN_SOURCE}/* ${MAPNIK_DIST}/${MAPNIK_PACKAGE_PREFIX}/
-install_name_tool -id @loader_path/${MAPNIK_PACKAGE_PREFIX}/lib/libmapnik.dylib ${MAPNIK_DIST}/${MAPNIK_PACKAGE_PREFIX}/lib/libmapnik.dylib
-tar cjf ${MAPNIK_DIST}/mapnik-${FOUND_VERSION}.tar.bz2 ${MAPNIK_PACKAGE_PREFIX}/
-COMMENT
