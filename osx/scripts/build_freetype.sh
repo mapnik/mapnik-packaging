674d54a6b12ffc78650a6686f056f9524f2e8cf2
Dane Springmeyer <dane@dbsgeo.com>
2014-01-24 17:37:53 -0800

    allow disabling of freetype CFF

--- osx/scripts/build_freetype.sh
+++ osx/scripts/build_freetype.sh
@@ -9,6 +9,13 @@ download freetype-${FREETYPE_VERSION}.tar.bz2
 echoerr 'building freetype'
 rm -rf freetype-${FREETYPE_VERSION}
 tar xf freetype-${FREETYPE_VERSION}.tar.bz2
+if [[ "${DISABLE_CFF:-false}" == true ]]; then
+    CFLAGS="${CFLAGS} -DCFF_CONFIG_OPTION_OLD_ENGINE"
+    echoerr "disabling freetype CFF"
+else
+    echoerr "keepin freetype CFF"
+fi
+
 cd freetype-${FREETYPE_VERSION}
 ./configure --prefix=${BUILD} --enable-static --disable-shared ${HOST_ARG} \
  --without-bzip2 \
