60f4067fced76f3a7d586a767797549268a0fe33
Dane Springmeyer <dane@dbsgeo.com>
2014-03-08 21:35:31 -0800

    fix #130 - refs mapnik/mapnik#2180

--- osx/scripts/build_postgres.sh
+++ osx/scripts/build_postgres.sh
@@ -26,6 +26,11 @@ cd ${PACKAGES}
 rm -rf postgresql-${POSTGRES_VERSION}
 tar xf postgresql-${POSTGRES_VERSION}.tar.bz2
 cd postgresql-${POSTGRES_VERSION}
+if [[ ${PLATFORM} == 'Linux' ]]; then
+    # https://github.com/mapnik/mapnik-packaging/issues/130
+    patch -N src/include/pg_config_manual.h ${PATCHES}/pg_config_manual.diff || true
+fi
+
 ./configure ${HOST_ARG} \
 --prefix=${BUILD} \
 --with-bonjour --with-openssl --with-pam --with-krb5 --with-gssapi --enable-thread-safety \
