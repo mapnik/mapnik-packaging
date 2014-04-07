6c355f12e2b84edf0363accb4ec88c5eb1e324e7
Dane Springmeyer <dane@dbsgeo.com>
2014-04-07 08:00:56 -0700

    build libgeotiff head

--- osx/scripts/build_geotiff.sh
+++ osx/scripts/build_geotiff.sh
@@ -4,12 +4,21 @@ set -o pipefail
 mkdir -p ${PACKAGES}
 cd ${PACKAGES}
 
-download libgeotiff-${LIBGEOTIFF_VERSION}.tar.gz
+#download libgeotiff-${LIBGEOTIFF_VERSION}.tar.gz
+
+if [ ! -d libgeotiff ]; then
+    svn co https://svn.osgeo.org/metacrs/geotiff/trunk/libgeotiff
+    cd libgeotiff
+    ./autogen.sh
+else
+    cd libgeotiff
+    make clean
+fi
 
 echoerr 'building geotiff'
-rm -rf libgeotiff-${LIBGEOTIFF_VERSION}
-tar xf libgeotiff-${LIBGEOTIFF_VERSION}.tar.gz
-cd libgeotiff-${LIBGEOTIFF_VERSION}
+#rm -rf libgeotiff-${LIBGEOTIFF_VERSION}
+#tar xf libgeotiff-${LIBGEOTIFF_VERSION}.tar.gz
+#cd libgeotiff-${LIBGEOTIFF_VERSION}
 ./configure --prefix=${BUILD} --enable-static --disable-shared --disable-dependency-tracking \
 --with-libtiff=${BUILD} \
 --with-jpeg=${BUILD} \
