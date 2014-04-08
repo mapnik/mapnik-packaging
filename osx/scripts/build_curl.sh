a53ce1c25c20da2451e233dc2b320fb394d97d60
Dane Springmeyer <dane@dbsgeo.com>
2014-04-08 12:28:13 -0400

    improve curl/openssl build + add root cert generation

--- osx/scripts/build_curl.sh
+++ osx/scripts/build_curl.sh
@@ -10,19 +10,35 @@ echo 'building curl'
 rm -rf curl-${CURL_VERSION}
 tar xf curl-${CURL_VERSION}.tar.bz2
 cd curl-${CURL_VERSION}
+
+# generate root certs
+# https://github.com/joyent/node/pull/6489
+CA_BUNDLE="${BUILD}/etc/openssl/certs/ca-bundle.crt"
+if [ ! -f ${CA_BUNDLE} ]; then
+    rm -f ./ca-bundle.crt
+    rm -f ./certdata.txt
+    perl ${PACKAGES}/curl-${CURL_VERSION}/lib/mk-ca-bundle.pl
+    if [[ ./ca-bundle.crt != "${CA_BUNDLE}" ]]; then
+        cp ./ca-bundle.crt ${CA_BUNDLE}
+    fi
+    rm -f ./ca-bundle.crt
+    rm -f ./certdata.txt
+fi
+
 ./configure --prefix=${BUILD} \
 --enable-static \
 --enable-shared \
 --enable-manual \
+--with-ssl=${BUILD} \
 --with-zlib=${ZLIB_PATH} \
+--without-ca-bundle \
+--without-ca-path \
 --without-darwinssl \
 --without-gnutls \
 --without-polarssl \
 --without-cyassl \
 --without-nss \
 --without-axtls \
---without-ca-bundle \
---without-ca-path \
 --without-libmetalink \
 --without-libssh2 \
 --without-librtmp \
@@ -51,4 +67,9 @@ cd curl-${CURL_VERSION}
 --disable-cookies
 make -j$JOBS
 make install
+
+# test https with cert
+echo ${BUILD}/bin/curl -I --cacert ${CA_BUNDLE} "https://www.mapbox.com/"
+${BUILD}/bin/curl -I --cacert ${CA_BUNDLE} "https://www.mapbox.com/"
+
 cd ${PACKAGES}
--- osx/scripts/build_openssl.sh
+++ osx/scripts/build_openssl.sh
@@ -7,15 +7,31 @@ cd ${PACKAGES}
 download openssl-${OPENSSL_VERSION}.tar.gz
 
 echoerr '*building openssl'
+
+if [[ ${UNAME} == 'Darwin' ]]; then
+    OS_COMPILER="darwin64-x86_64-cc"
+elif [[ ${UNAME} == 'Linux' ]]; then
+    OS_COMPILER="linux-x86_64"
+else
+    echoerr "unknown os/compiler version for your platform ${UNAME}"
+fi
+
 rm -rf openssl-${OPENSSL_VERSION}
 tar xf openssl-${OPENSSL_VERSION}.tar.gz
 cd openssl-${OPENSSL_VERSION}
-./Configure --prefix=${BUILD} \
---openssldir=${BUILD}/etc/openssl \
-zlib-dynamic \
+./Configure \
+--prefix=${BUILD} \
+no-idea \
+no-mdc2 \
+no-rc5 \
+no-zlib \
 no-shared \
-darwin64-x86_64-cc \
+enable-tlsext \
+no-ssl2 \
+--openssldir=${BUILD}/etc/openssl \
+${OS_COMPILER} \
 enable-ec_nistp_64_gcc_128
+
 make depend
 # now re-configure to put $CFLAGS
 # we do this now to avoid breaking 'make depend'
@@ -26,7 +42,8 @@ no-shared \
 darwin64-x86_64-cc \
 enable-ec_nistp_64_gcc_128 \
 "$CFLAGS"
+
 make
-#make -j${JOBS}
-#make install
+make install
+
 cd ${PACKAGES}
