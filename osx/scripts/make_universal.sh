5f1dbeefbcb6b5658ff4eda348cc4300d4cd03be
Konstantin Käfer <mail@kkaefer.com>
2014-04-08 20:18:51 -0400

    copy files before calling lipo

--- osx/scripts/make_universal.sh
+++ osx/scripts/make_universal.sh
@@ -7,15 +7,21 @@ if [[ $UNAME == 'Darwin' ]]; then
     echo '*making universal libs*'
     ARCHS="x86_64 arm64 armv7s armv7 i386"
     LIBS=$(find ${ROOTDIR}/out/*/lib -maxdepth 1 -name '*.a' -exec basename '{}' \; | sort | uniq)
+
+    for arch in ${ARCHS}; do
+        if [ -f "${BUILD_ROOT}-${arch}" ]; then
+            cp -r "${BUILD_ROOT}-${arch}/" "${BUILD_ROOT}-universal"
+            build_root_escaped=$(echo "${BUILD_ROOT}" | sed -e 's/[]\/()$*.^|[]/\\&/g')
+            find ${BUILD_ROOT}-universal/ \( -name "*.pc" -or -name "*.la" -or -name "*-config" \) \
+                -exec sed -i '' "s/${build_root_escaped}-${arch}/${build_root_escaped}-universal/g" {} \;
+        fi
+    done;
+
     for libname in ${LIBS}; do
         echo '*making universal '$libname'*'
         FROM_LIBS=""
         for arch in ${ARCHS}; do
             if [ -f "${BUILD_ROOT}-${arch}/lib/${libname}" ]; then
-                cp -r "${BUILD_ROOT}-${arch}/" "${BUILD_ROOT}-universal"
-                build_root_escaped=$(echo "${BUILD_ROOT}" | sed -e 's/[]\/()$*.^|[]/\\&/g')
-                find ${BUILD_ROOT}-universal/ \( -name "*.pc" -or -name "*.la" -or -name "*-config" \) \
-                    -exec sed -i '' "s/${build_root_escaped}-${arch}/${build_root_escaped}-universal/g" {} \;
                 FROM_LIBS="$FROM_LIBS ${BUILD_ROOT}-${arch}/lib/${libname}"
             fi
         done;
