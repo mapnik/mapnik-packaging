fbb5aef15df9736530329dd7f890aec1d96cfdb6
Dane Springmeyer <dane@mapbox.com>
2014-04-11 10:26:15 -0400

    target the active platform on osx/ios dynamically

--- osx/MacOSX.sh
+++ osx/MacOSX.sh
@@ -8,6 +8,7 @@ export BOOST_ARCH="x86"
 export ARCH_NAME="x86_64"
 export HOST_ARG="--host x86_64-apple-darwin"
 export ACTIVE_SDK_VERSION="10.8"
+export ACTIVE_SDK_VERSION="`xcrun --sdk macosx --show-sdk-version`"
 if [[ "${CXX11:-false}" != false ]]; then
   export MIN_SDK_VERSION_FLAG="-mmacosx-version-min=10.7"
 else
