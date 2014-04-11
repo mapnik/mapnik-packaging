2bdf2a88ee4fe325b3fd07133c617dcb296cb1ad
Dane Springmeyer <dane@mapbox.com>
2014-04-11 11:48:50 -0400

    improve boost header approach to use bcp to dynamically install just the required headers

--- osx/scripts/build_boost.sh
+++ osx/scripts/build_boost.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 if [ -z "$@" ]; then
-  echo "please pass boost library names like '--with-thread' or just 'header_only' to only setup boost and not build any libs"
+  echo "please pass boost library names like '--with-thread' or just a path to headers that use boost (bcp will search them)"
   exit 1
 fi
 
-LIBRARY_NAMES="$@"
+TARGET_NAMES="$@"
 
 set -e -u
 set -o pipefail
@@ -60,42 +60,43 @@ fi
 # HINT: problems with icu configure check?
 # cat bin.v2/config.log to see problems
 
-if [ $BOOST_ARCH = "arm" ]; then
-    CROSS_FLAGS=""
-else
-    CROSS_FLAGS="tools/bcp"
-fi
-
-# TODO set address-model ?
-
-# only build with icudata library support on mac
-if [ ${BOOST_ARCH} = "x86" ]; then
-    BOOST_LDFLAGS="${STDLIB_LDFLAGS} ${LDFLAGS} -L${BUILD}/lib -licuuc -licui18n -licudata"
-    BOOST_CXXFLAGS="${STDLIB_CXXFLAGS} ${CXXFLAGS} ${ICU_CORE_CPP_FLAGS}"
-    ICU_DETAILS="-sHAVE_ICU=1 -sICU_PATH=${BUILD}"
-else
-    mv libs/regex/build/has_icu_test.cpp libs/regex/build/has_icu_test.cpp_
-    echo '#error' > libs/regex/build/has_icu_test.cpp
-    BOOST_LDFLAGS="${STDLIB_LDFLAGS} ${LDFLAGS}"
-    BOOST_CXXFLAGS="${STDLIB_CXXFLAGS} ${CXXFLAGS} ${ICU_EXTRA_CPP_FLAGS}"
-    ICU_DETAILS=""
-fi
-
-if [ $PLATFORM = 'Android' ]; then
-    # TODO - fixed in boost 1.55: https://svn.boost.org/trac/boost/changeset/85251
-    # workaround libs/filesystem/src/operations.cpp:77:30: fatal error: sys/statvfs.h: No such file or directory
-    mkdir -p tmp/sys/
-    echo '#include <sys/statfs.h>' > tmp/sys/statvfs.h
-    echo '#define statvfs statfs' >> tmp/sys/statvfs.h
-    BOOST_CXXFLAGS="${BOOST_CXXFLAGS} -I./tmp"
-    BOOST_LDFLAGS="${BOOST_LDFLAGS} -L./tmp"
-fi
-
 B2_VERBOSE="-d0"
 #B2_VERBOSE="-d2"
 echoerr 'compiling boost'
 
-if [[ ${LIBRARY_NAMES} != 'header_only' ]] && [[ ${LIBRARY_NAMES} != 'none' ]]; then
+# if we've requested libraries
+if test "${TARGET_NAMES#*'--with'}" != "${TARGET_NAMES}"; then
+
+  if [ $BOOST_ARCH = "arm" ]; then
+      CROSS_FLAGS=""
+  else
+      CROSS_FLAGS="tools/bcp"
+  fi
+
+  # TODO set address-model ?
+
+  # only build with icudata library support on mac
+  if [ ${BOOST_ARCH} = "x86" ]; then
+      BOOST_LDFLAGS="${STDLIB_LDFLAGS} ${LDFLAGS} -L${BUILD}/lib -licuuc -licui18n -licudata"
+      BOOST_CXXFLAGS="${STDLIB_CXXFLAGS} ${CXXFLAGS} ${ICU_CORE_CPP_FLAGS}"
+      ICU_DETAILS="-sHAVE_ICU=1 -sICU_PATH=${BUILD}"
+  else
+      mv libs/regex/build/has_icu_test.cpp libs/regex/build/has_icu_test.cpp_
+      echo '#error' > libs/regex/build/has_icu_test.cpp
+      BOOST_LDFLAGS="${STDLIB_LDFLAGS} ${LDFLAGS}"
+      BOOST_CXXFLAGS="${STDLIB_CXXFLAGS} ${CXXFLAGS} ${ICU_EXTRA_CPP_FLAGS}"
+      ICU_DETAILS=""
+  fi
+
+  if [ $PLATFORM = 'Android' ]; then
+      # TODO - fixed in boost 1.55: https://svn.boost.org/trac/boost/changeset/85251
+      # workaround libs/filesystem/src/operations.cpp:77:30: fatal error: sys/statvfs.h: No such file or directory
+      mkdir -p tmp/sys/
+      echo '#include <sys/statfs.h>' > tmp/sys/statvfs.h
+      echo '#define statvfs statfs' >> tmp/sys/statvfs.h
+      BOOST_CXXFLAGS="${BOOST_CXXFLAGS} -I./tmp"
+      BOOST_LDFLAGS="${BOOST_LDFLAGS} -L./tmp"
+  fi
     ./b2 ${CROSS_FLAGS} \
       --prefix=${BUILD} -j${JOBS} ${B2_VERBOSE} \
       --ignore-site-config --user-config=user-config.jam \
@@ -113,5 +114,20 @@ if [[ ${LIBRARY_NAMES} != 'header_only' ]] && [[ ${LIBRARY_NAMES} != 'none' ]]; 
     check_and_clear_libs
     echoerr 'done compiling boost'
 else
-    echoerr 'boost setup, but skipped compiling any boost libraries'
+    if [[ $BOOST_ARCH != "arm" ]]; then
+      echoerr 'building bcp'
+      ./b2 tools/bcp -j${JOBS} ${B2_VERBOSE} \
+        --ignore-site-config --user-config=user-config.jam \
+        toolset="${BOOST_TOOLSET}"
+      echoerr 'installing headers with bcp'
+      STAGING_DIR=bcp_staging
+      mkdir -p ${STAGING_DIR}
+      rm -rf ${STAGING_DIR}/*
+      ./dist/bin/bcp "${TARGET_NAMES}" ${STAGING_DIR} 1>/dev/null
+      du -h -d 0 ${STAGING_DIR}/boost/
+      mkdir -p ${BUILD}/include
+      cp -r ${STAGING_DIR}/boost ${BUILD}/include/
+    else
+       echoerr 'skipping installing headers with bcp because we are cross compiling'
+    fi
 fi
