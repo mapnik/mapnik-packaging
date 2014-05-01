071638238e5a1a326c6679a0d3cf6340d3861a8b
Dane Springmeyer <dane@mapbox.com>
2014-05-01 15:53:07 -0700

    fix harfbuzz install

--- osx/scripts/build_harfbuzz.sh
+++ osx/scripts/build_harfbuzz.sh
@@ -24,6 +24,6 @@ LDFLAGS="${STDLIB_LDFLAGS} ${LDFLAGS}"
  --with-freetype \
  --with-uniscribe=no \
  --with-coretext=no
-$$MAKE -j${JOBS}
-$$MAKE install
+$MAKE -j${JOBS}
+$MAKE install
 cd ${PACKAGES}
