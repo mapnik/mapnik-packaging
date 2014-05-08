mapnik/mapnik-packaging

commit 40413db8de4a54a4fbfe2f43f0545f3217f77149

make as a variable

osx/scripts/build_openssl.sh
@@ -10,10 +10,10 @@ echoerr '*building openssl'
 
 if [[ ${UNAME} == 'Darwin' ]]; then
     OS_COMPILER="darwin64-x86_64-cc"
-    MAKEDEPEND="makedepend"
+    MAKEDEPEND="$makedepend"
 elif [[ ${UNAME} == 'Linux' ]]; then
     OS_COMPILER="linux-x86_64"
-    MAKEDEPEND="gccmakedep"
+    MAKEDEPEND="gmakedepend"
 else
     echoerr "unknown os/compiler version for your platform ${UNAME}"
 fi
@@ -34,12 +34,12 @@ no-ssl2 \
 ${OS_COMPILER} \
 enable-ec_nistp_64_gcc_128
 
-make depend MAKEDEPPROG=${MAKEDEPEND}
+$MAKE depend $MAKEDEPPROG=${$MAKEDEPEND}
 
 # now re-configure to apply custom $CFLAGS
 CFLAGS="-DOPENSSL_NO_DEPRECATED -DOPENSSL_NO_COMP -DOPENSSL_NO_HEARTBEATS $CFLAGS"
 
-# we do this now to avoid breaking 'make depend'
+# we do this now to avoid breaking '$MAKE depend'
 ./Configure --prefix=${BUILD} \
 --openssldir=${BUILD}/etc/openssl \
 zlib-dynamic \
@@ -48,7 +48,7 @@ ${OS_COMPILER} \
 enable-ec_nistp_64_gcc_128 \
 "$CFLAGS"
 
-make
-make install
+$MAKE
+$MAKE install
 
 cd ${PACKAGES}

.travis.yml
@@ -6,13 +6,13 @@ compiler:
 env:
   matrix:
     - CXX11=true TARGET=build_osmium
-#    - CXX11=true TARGET=build_osrm QUIET=true
-#    - CXX11=false TARGET=build_osrm QUIET=true
-#    - CXX11=true TARGET=build_mapnik PUBLISH=true QUIET=true
-#    - CXX11=false TARGET=build_mapnik PUBLISH=true QUIET=true
-#    - CXX11=false TARGET=build_osm2pgsql QUIET=true
-#    - CXX11=false TARGET=build_liblas QUIET=true
-#    - CXX11=false TARGET=build_http QUIET=true
+    - CXX11=true TARGET=build_osrm QUIET=true
+    - CXX11=false TARGET=build_osrm QUIET=true
+    - CXX11=true TARGET=build_mapnik PUBLISH=true QUIET=true
+    - CXX11=false TARGET=build_mapnik PUBLISH=true QUIET=true
+    - CXX11=false TARGET=build_osm2pgsql QUIET=true
+    - CXX11=false TARGET=build_liblas QUIET=true
+    - CXX11=false TARGET=build_http QUIET=true
   global:
     - secure: jLa/ai9FzqYRZkZrjQhn5KbsSrDDLBV2hPlKQh/nMn5bb5RooMnAo/9aSYsq8o4lMtnqsYX7wv/r35GKpJJk1OFBWmj1L94KTfm1TdC5Xlr5iumRbQ9VEPBbMgQw6WU+bp88QxvT0I9DZxgciMk6GV9eBM3dH4BSIwRCamOl018=
     - secure: iQ5aDVTqRYX8YQdxi8gk0jLoNtWvtb7x3ZQORQT9w+y/X4qX+5mV/FV88BQBYF4MMlF5ePGx8IItpEagllIZboAf68CBvclretlPXUiw3JyHaALfHJI1n02y/j/NnY2P3QJUG9QuFBdIAeUWUyReGxOD9Bhcj4E8nHkAgDNsz5k=

osx/Android.sh
@@ -8,7 +8,7 @@ export BOOST_ARCH="arm"
 export ARCH_NAME="gcc-arm"
 export HOST_ARG="--host=arm-linux-androideabi"
 source $(dirname "$BASH_SOURCE")/settings.sh
-
+export MAKE="make"
 if [[ "${CXX11:-false}" == false ]]; then
   export CXX11=false
 fi

osx/FreeBSD.sh
@@ -8,8 +8,8 @@ export BOOST_ARCH="x86"
 export ARCH_NAME="gcc-x86_64"
 export HOST_ARG=""
 export CXX="clang++"
-export MAKE="gmake"
 if [[ "${CXX11:-false}" == false ]]; then
   export CXX11=false
 fi
+export MAKE="gmake"
 source $(dirname "$BASH_SOURCE")/settings.sh

osx/Linaro.sh
@@ -10,7 +10,7 @@ export BOOST_ARCH="arm"
 export ARCH_NAME="gcc-arm-linux-gnueabihf"
 export HOST_ARG="--host=arm-linux-gnueabihf"
 source $(dirname "$BASH_SOURCE")/settings.sh
-
+export MAKE="make"
 # default to C++11 for this platform
 if [[ "${CXX11:-false}" == false ]]; then
   export CXX11=true

osx/Linux.sh
@@ -10,4 +10,5 @@ export HOST_ARG=""
 if [[ "${CXX11:-false}" == false ]]; then
   export CXX11=false
 fi
+export MAKE="make"
 source $(dirname "$BASH_SOURCE")/settings.sh

osx/MacOSX.sh
@@ -14,5 +14,6 @@ else
   export MIN_SDK_VERSION_FLAG="-mmacosx-version-min=10.6"
   export CXX11=false
 fi
+export MAKE="make"
 #export MACOSX_DEPLOYMENT_TARGET=${MIN_SDK_VERSION} # breaks distutils
 source $(dirname "$BASH_SOURCE")/settings.sh

osx/iPhoneOS.sh
@@ -9,7 +9,7 @@ export ARCH_NAME="armv7"
 export HOST_ARG="--host=arm-apple-darwin"
 export ACTIVE_SDK_VERSION="`xcrun --sdk iphoneos --show-sdk-version`"
 export MIN_SDK_VERSION_FLAG="-miphoneos-version-min=${ACTIVE_SDK_VERSION}"
-
+export MAKE="make"
 if [[ "${CXX11:-false}" == false ]]; then
   export CXX11=false
 fi

commit f4c5c192042258ead566ee0121009439fdb0f39b

fix openssl commands - regressed in 40413db8de

osx/scripts/build_openssl.sh
@@ -10,10 +10,10 @@ echoerr '*building openssl'
 
 if [[ ${UNAME} == 'Darwin' ]]; then
     OS_COMPILER="darwin64-x86_64-cc"
-    MAKEDEPEND="$makedepend"
+    MAKEDEPEND="makedepend"
 elif [[ ${UNAME} == 'Linux' ]]; then
     OS_COMPILER="linux-x86_64"
-    MAKEDEPEND="gmakedepend"
+    MAKEDEPEND="gccmakedep"
 else
     echoerr "unknown os/compiler version for your platform ${UNAME}"
 fi
@@ -34,7 +34,7 @@ no-ssl2 \
 ${OS_COMPILER} \
 enable-ec_nistp_64_gcc_128
 
-$MAKE depend $MAKEDEPPROG=${$MAKEDEPEND}
+$MAKE depend MAKEDEPPROG=${MAKEDEPEND}
 
 # now re-configure to apply custom $CFLAGS
 CFLAGS="-DOPENSSL_NO_DEPRECATED -DOPENSSL_NO_COMP -DOPENSSL_NO_HEARTBEATS $CFLAGS"