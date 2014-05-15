55bc9efe62293df8e3420231259c3a31653257c8
Dane Springmeyer <dane@mapbox.com>
2014-05-14 17:40:29 -0700

    if we build zlib, build statically

--- osx/scripts/build_zlib.sh
+++ osx/scripts/build_zlib.sh
@@ -12,14 +12,10 @@ else
     rm -rf zlib-${ZLIB_VERSION}
     tar xf zlib-${ZLIB_VERSION}.tar.gz
     cd zlib-${ZLIB_VERSION}
-    # no longer needed on os x with zlib 1.2.8
-    #if [ $UNAME = 'Darwin' ]; then
-    #  patch -N < ${PATCHES}/zlib-configure.diff
-    #fi
     if [ ${PLATFORM} = 'Android' ]; then
        patch -N < ${PATCHES}/android-zlib.diff
     fi
-    ./configure --prefix=${BUILD}
+    ./configure --prefix=${BUILD} --static
     $MAKE -j$JOBS
     $MAKE install
     cd ${PACKAGES}
