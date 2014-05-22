ada135d13c4a114035c94c2d150a61250ebebc74
Dane Springmeyer <dane@mapbox.com>
2014-05-21 18:12:32 -0700

    install automake autoconf

--- build.sh
+++ build.sh
@@ -42,7 +42,7 @@ function prep_linux {
     sudo apt-get update -y -qq
   fi;
   echo "installing build tools"
-  sudo apt-get install -qq -y build-essential git cmake zlib1g-dev unzip autotools-dev
+  sudo apt-get install -qq -y build-essential git cmake zlib1g-dev unzip make libtool autotools-dev automake
   mkdir -p ${BUILD}
   mkdir -p ${BUILD}/lib
   mkdir -p ${BUILD}/include
