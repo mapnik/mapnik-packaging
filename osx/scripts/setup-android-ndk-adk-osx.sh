894d4f5b6700585ea40e3ded1ca671133ec20706
Dane Springmeyer <dane@mapbox.com>
2014-06-04 23:18:29 -0700

    avoid first run undefined variable problem in android setup script

--- osx/scripts/setup-android-ndk-adk-osx.sh
+++ osx/scripts/setup-android-ndk-adk-osx.sh
@@ -5,15 +5,15 @@ mkdir -p ${PACKAGES}
 cd ${PACKAGES}
 
 if [[ ! -f "android-ndk-${ANDROID_NDK_VERSION}-${platform}-x86_64.tar.bz2" ]]; then
-    echoerr "downloading http://dl.google.com/android/ndk/android-ndk-${ANDROID_NDK_VERSION}-${platform}-x86_64.tar.bz2"
-    ${SYSTEM_CURL} -s -S -f -O -L http://dl.google.com/android/ndk/android-ndk-${ANDROID_NDK_VERSION}-${platform}-x86_64.tar.bz2
+    echo "downloading http://dl.google.com/android/ndk/android-ndk-${ANDROID_NDK_VERSION}-${platform}-x86_64.tar.bz2"
+    curl -s -S -f -O -L http://dl.google.com/android/ndk/android-ndk-${ANDROID_NDK_VERSION}-${platform}-x86_64.tar.bz2
 else
-    echoerr "using cached http://dl.google.com/android/ndk/android-ndk-${ANDROID_NDK_VERSION}-${platform}-x86_64.tar.bz2"
+    echo "using cached http://dl.google.com/android/ndk/android-ndk-${ANDROID_NDK_VERSION}-${platform}-x86_64.tar.bz2"
 fi
 
 if [[ ! -d "android-ndk-${ANDROID_NDK_VERSION}" ]]; then
-    echoerr "unpacking android-ndk-${ANDROID_NDK_VERSION}-${platform}-x86_64.tar.bz2"
+    echo "unpacking android-ndk-${ANDROID_NDK_VERSION}-${platform}-x86_64.tar.bz2"
     tar xf android-ndk-${ANDROID_NDK_VERSION}-${platform}-x86_64.tar.bz2
 else
-    echoerr "using cached android-ndk-${ANDROID_NDK_VERSION}-${platform}-x86_64.tar.bz2"
+    echo "using cached android-ndk-${ANDROID_NDK_VERSION}-${platform}-x86_64.tar.bz2"
 fi
