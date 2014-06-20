d52c5d15665ddf4c6b167637ea626f60d6341b6a
Dane Springmeyer <dane@mapbox.com>
2014-06-20 14:38:51 -0700

    enable setting custom compiler in tbb makefiles

--- osx/scripts/build_tbb.sh
+++ osx/scripts/build_tbb.sh
@@ -17,6 +17,7 @@ if [[ $CXX11 == true ]]; then
     rm -rf tbb42_20140416oss
     tar xf tbb42_20140416oss_src.tgz
     cd tbb42_20140416oss
+    patch -N -p1 <  ${PATCHES}/tbb_compiler_override.diff || true
     # note: static linking not allowed: http://www.threadingbuildingblocks.org/faq/11
     if [[ $UNAME == 'Darwin' ]]; then
     $MAKE -j${JOBS} tbb_build_prefix=BUILDPREFIX arch=intel64 cpp0x=1 stdlib=libc++ compiler=clang tbb_build_dir=$(pwd)/build
