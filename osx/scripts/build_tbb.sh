c3c9f82c0ffcc7f590bdc803cdcaf64698d7b6b6
Dane Springmeyer <dane@mapbox.com>
2014-06-20 11:46:59 -0700

    improve libtbb build

---
+++ osx/scripts/build_tbb.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+set -e -u
+set -o pipefail
+mkdir -p ${PACKAGES}
+cd ${PACKAGES}
+
+if [ ! -f tbb42_20140416oss_src.tgz ]; then
+    echoerr "downloading intel tbb"
+    curl -s -S -f -O -L https://www.threadingbuildingblocks.org/sites/default/files/software_releases/source/tbb42_20140416oss_src.tgz
+else
+    echoerr "using cached node at tbb42_20140416oss_src.tgz"
+fi
+
+echoerr 'building tbb'
+
+if [[ $CXX11 == true ]]; then
+    rm -rf tbb42_20140416oss
+    tar xf tbb42_20140416oss_src.tgz
+    cd tbb42_20140416oss
+    # note: static linking not allowed: http://www.threadingbuildingblocks.org/faq/11
+    if [[ $UNAME == 'Darwin' ]]; then
+    $MAKE -j${JOBS} tbb tbb_build_prefix=BUILDPREFIX arch=intel64 cpp0x=1 stdlib=libc++ compiler=clang tbb_build_dir=$(pwd)/build
+    else
+    $MAKE -j${JOBS} tbb tbb_build_prefix=BUILDPREFIX arch=intel64 cpp0x=1 stdlib=libc++ tbb_build_dir=$(pwd)/build
+    fi
+
+    # custom install
+    cp $(pwd)/build/BUILDPREFIX_release/libtbb.dylib $BUILD/lib/
+    cp -r $(pwd)/include/tbb ${BUILD}/include/
+else
+    echoerr 'skipping libtbb build since we only target c++11 mode'
+fi
