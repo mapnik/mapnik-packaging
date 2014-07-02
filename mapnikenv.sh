b9f82ab46ce73d4aaa7fe44ecff592fc2ff7cd22
Dane Springmeyer <dane@mapbox.com>
2014-07-02 15:18:01 -0700

    add to mapnikenv.sh script enough to get python bindings working

--- mapnikenv.sh
+++ mapnikenv.sh
@@ -27,6 +27,12 @@ if [[ -d ${MAPNIK_BIN_SOURCE} ]]; then
     export C_INCLUDE_PATH="${MAPNIK_BIN_SOURCE}/include"
     export CPLUS_INCLUDE_PATH="${MAPNIK_BIN_SOURCE}/include"
     export PATH="${MAPNIK_BIN_SOURCE}/bin:${PATH}"
+    export PYTHONPATH="${MAPNIK_BIN_SOURCE}/lib/python2.7/site-packages"
+    export ICU_DATA="${MAPNIK_BIN_SOURCE}/share/mapnik/icu"
+    export GDAL_DATA="${MAPNIK_BIN_SOURCE}/share/mapnik/gdal"
+    export PROJ_LIB="${MAPNIK_BIN_SOURCE}/share/mapnik/proj"
+    export MAPNIK_FONT_DIRECTORY="${MAPNIK_BIN_SOURCE}/lib/mapnik/fonts/dejavu-fonts-ttf-2.33/ttf/"
+    export MAPNIK_INPUT_PLUGINS_DIRECTORY="${MAPNIK_BIN_SOURCE}/lib/mapnik/input/"
 else
     echo "Error mapnik build directory does not exist:"
     echo "    ${MAPNIK_BIN_SOURCE}"
