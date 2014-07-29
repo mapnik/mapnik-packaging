b39d3e9d22e669e776c45865a0e212f371b85bd8
Dane Springmeyer <dane@mapbox.com>
2014-07-29 12:24:30 -0700

    add cryptocpp

--- build.sh
+++ build.sh
@@ -189,6 +189,7 @@ function build_osmium {
   ./scripts/build_boost.sh --with-test --with-program_options
   b ./scripts/build_protobuf.sh
   b ./scripts/build_osm-pbf.sh
+  b ./scripts/build_cryptopp.sh
   teardown
 }
 
