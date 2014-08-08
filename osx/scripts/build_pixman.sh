7c5ae48acb6c7be28d958da5d8a4ab114fbdc116
Dane Springmeyer <dane@mapbox.com>
2014-08-08 08:19:42 -0700

    fix osx pixman build

--- osx/scripts/build_pixman.sh
+++ osx/scripts/build_pixman.sh
@@ -13,12 +13,14 @@ cd pixman-${PIXMAN_VERSION}
 ./configure --enable-static --disable-shared \
 --disable-dependency-tracking --prefix=${BUILD} \
 --disable-mmx --disable-ssse3 --disable-libpng
-$MAKE -j${JOBS}
-$MAKE install
+
+# note: --disable-ssse3 is required to fix clang -flto on linux
+# LLVM ERROR: Cannot select: intrinsic %llvm.x86.ssse3.pabs.w.128
+
+# set +e is to workaround osx bug in pixman tests: Undefined symbols for architecture x86_64: "_prng_state
+set +e
+$MAKE -j${JOBS} -i -k
+$MAKE install -i -k
+set -e
 cd ${PACKAGES}
 
-: '
---disable-ssse3 is required to fix clang -flto on linux
-
-LLVM ERROR: Cannot select: intrinsic %llvm.x86.ssse3.pabs.w.128
-'
