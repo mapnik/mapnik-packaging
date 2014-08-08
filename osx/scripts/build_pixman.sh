69b78838681bb24e41be8ab098c62d7f6ce4b03b
Dane Springmeyer <dane@mapbox.com>
2014-08-07 18:53:39 -0700

    fix pixman build

--- osx/scripts/build_pixman.sh
+++ osx/scripts/build_pixman.sh
@@ -12,19 +12,13 @@ tar xf pixman-${PIXMAN_VERSION}.tar.gz
 cd pixman-${PIXMAN_VERSION}
 ./configure --enable-static --disable-shared \
 --disable-dependency-tracking --prefix=${BUILD} \
---disable-mmx
-set +e
-$MAKE -j${JOBS} -i -k
-$MAKE install -i -k
-set -e
+--disable-mmx --disable-ssse3 --disable-libpng
+$MAKE -j${JOBS}
+$MAKE install
 cd ${PACKAGES}
 
 : '
-On linux with clang-3.4 and -flto:
-
-perhaps: http://llvm.org/bugs/show_bug.cgi?id=13000
+--disable-ssse3 is required to fix clang -flto on linux
 
 LLVM ERROR: Cannot select: intrinsic %llvm.x86.ssse3.pabs.w.128
-clang: error: linker command failed with exit code 1 (use -v to see invocation)
-
 '
