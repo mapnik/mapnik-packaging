11c93d31c73b925fdf1af750785d293173565f2a
Dane Springmeyer <dane@mapbox.com>
2014-08-08 08:27:28 -0700

    also disable gtk in pixman build

--- osx/scripts/build_pixman.sh
+++ osx/scripts/build_pixman.sh
@@ -12,7 +12,7 @@ tar xf pixman-${PIXMAN_VERSION}.tar.gz
 cd pixman-${PIXMAN_VERSION}
 ./configure --enable-static --disable-shared \
 --disable-dependency-tracking --prefix=${BUILD} \
---disable-mmx --disable-ssse3 --disable-libpng
+--disable-mmx --disable-ssse3 --disable-libpng --disable-gtk \
 
 # note: --disable-ssse3 is required to fix clang -flto on linux
 # LLVM ERROR: Cannot select: intrinsic %llvm.x86.ssse3.pabs.w.128
