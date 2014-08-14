256cd60f09c7596f2f4ac7dbb2fb482d70fcf90b
Dane Springmeyer <dane@mapbox.com>
2014-08-14 12:26:03 -0700

    upgrade harfbuzz 0.9.34->0.9.35

--- osx/settings.sh
+++ osx/settings.sh
@@ -466,7 +466,7 @@ export NOSE_VERSION="1.2.1"
 export SPARSEHASH_VERSION="2.0.2"
 # http://www.freedesktop.org/software/harfbuzz/release/
 # bz2
-export HARFBUZZ_VERSION="0.9.34"
+export HARFBUZZ_VERSION="0.9.35"
 export STXXL_VERSION="1.4.0"
 export LUABIND_VERSION="0.9.1"
 export LUA_VERSION="5.1.5"
