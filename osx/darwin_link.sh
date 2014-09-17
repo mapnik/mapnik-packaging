cc2828113493075e3b962501f45f030fa41f5c4b
Justin R. Miller <incanus@codesorcery.net>
2014-09-16 19:05:52 -0700

    re-run configure post-symlink

--- osx/darwin_link.sh
+++ osx/darwin_link.sh
@@ -4,14 +4,22 @@ export ROOTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 OUTPUT="${ROOTDIR}/out/build-cpp11-libcpp-universal"
 
+# update the symlink for the proper platform
 if [ -d ${OUTPUT} ] && [ ! -L ${OUTPUT} ]; then
     mv ${OUTPUT} "${ROOTDIR}/out/universal"
 elif [ -L ${OUTPUT} ]; then
     rm ${OUTPUT}
 fi
 
-if [ "$1" = "ios" ]; then
+if [[ "$1" == "ios" ]]; then
     ln -s "${ROOTDIR}/out/universal" ${OUTPUT}
 else
     ln -s "${ROOTDIR}/out/build-cpp11-libcpp-x86_64-macosx" ${OUTPUT}
 fi
+
+# re-run configure against the new build products
+./configure \
+--pkg-config-root=`pwd`/mapnik-packaging/osx/out/build-cpp11-libcpp-universal/lib/pkgconfig \
+--boost=`pwd`/mapnik-packaging/osx/out/build-cpp11-libcpp-x86_64-macosx \
+--npm=`which npm` \
+--node=`which node`
