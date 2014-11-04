e6f0dabac764ba5d3f3f17cfdf670e398db5edd9
Dane Springmeyer <dane@mapbox.com>
2014-11-04 08:08:14 -0500

    ensure realpath is available when settings is sourced [skip ci]

--- build.sh
+++ build.sh
@@ -158,13 +158,13 @@ function upgrade_compiler {
 
 function prep_linux {
   cd osx
+  echo "installing build tools"
+  sudo apt-get install -qq -y curl build-essential git cmake zlib1g-dev unzip make libtool autotools-dev automake realpath autoconf ragel
   if [[ "${MASON_PLATFORM:-false}" != false ]]; then
       source ${MASON_PLATFORM}.sh
   else
       source Linux.sh
   fi
-  echo "installing build tools"
-  sudo apt-get install -qq -y curl build-essential git cmake zlib1g-dev unzip make libtool autotools-dev automake realpath autoconf ragel
 }
 
 function prep_osx {
