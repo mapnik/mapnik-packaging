408892d90d6cc6bbe0975001b6901766a155d74e
Dane Springmeyer <dane@mapbox.com>
2014-11-04 12:05:31 -0500

    use 7z to unpack android ndk

--- osx/scripts/setup-android-ndk.sh
+++ osx/scripts/setup-android-ndk.sh
@@ -6,6 +6,10 @@ cd ${PACKAGES}
 
 export uname_lowercase=$(echo $(uname -s) | sed "y/ABCDEFGHIJKLMNOPQRSTUVWXYZ/abcdefghijklmnopqrstuvwxyz/")
 
+if [[ $uname_lowercase == 'linux' ]]; then
+    sudo apt-get install -y p7zip-full
+fi
+
 if [[ ! -f "android-ndk-${MASON_ANDROID_NDK_VERSION}-${uname_lowercase}-x86_64.bin" ]]; then
     echo "downloading http://dl.google.com/android/ndk/android-ndk-${MASON_ANDROID_NDK_VERSION}-${uname_lowercase}-x86_64.bin"
     curl -s -S -f -O -L http://dl.google.com/android/ndk/android-ndk-${MASON_ANDROID_NDK_VERSION}-${uname_lowercase}-x86_64.bin
@@ -16,7 +20,7 @@ fi
 if [[ ! -d "android-ndk-${MASON_ANDROID_NDK_VERSION}" ]]; then
     echo "unpacking $(pwd)/android-ndk-${MASON_ANDROID_NDK_VERSION}-${uname_lowercase}-x86_64.bin"
     chmod a+x android-ndk-${MASON_ANDROID_NDK_VERSION}-${uname_lowercase}-x86_64.bin
-    ./android-ndk-${MASON_ANDROID_NDK_VERSION}-${uname_lowercase}-x86_64.bin
+    7z x ./android-ndk-${MASON_ANDROID_NDK_VERSION}-${uname_lowercase}-x86_64.bin
 else
     echo "using cached $(pwd)/android-ndk-${MASON_ANDROID_NDK_VERSION}-${uname_lowercase}-x86_64.bin"
 fi
