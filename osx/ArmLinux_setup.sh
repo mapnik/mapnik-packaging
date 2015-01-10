c7b3275edafed61aca7e5fac6116140f51cfe8cc
Brandon Anderson <bander9289+pub@gmail.com>
2015-01-09 18:15:10 -0600

    Correct path to stage2 setup script

--- osx/ArmLinux_setup.sh
+++ osx/ArmLinux_setup.sh
@@ -6,7 +6,7 @@ export COMPILE_ENV="sudo chroot $CHROOT_BASE"
 sudo apt-get install debootstrap qemu-user-static
 sudo debootstrap --foreign --variant=minbase --arch armhf --include=locales testing `pwd`/$CHROOT_BASE http://mirrordirector.raspbian.org/raspbian
 sudo cp /usr/bin/qemu-arm-static $CHROOT_BASE/usr/bin/
-sudo cp ../osx/ArmLinux_setup_stage2.sh $CHROOT_BASE/tmp/setup.sh # FIXME: '../'
+sudo cp osx/ArmLinux_setup_stage2.sh $CHROOT_BASE/tmp/setup.sh
 
 # enter chroot/qemu
 $COMPILE_ENV /tmp/setup.sh
