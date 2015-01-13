8a39ec9cf6e295871f165b7e13401566d822a975
Brandon Anderson <bander9289+pub@gmail.com>
2015-01-13 12:09:41 -0600

    Force arm build to use g++, since upgrade_clang forces libstdc++ upgrade which ends with g++-4.9 instead of g++4.8

--- osx/ArmLinux.sh
+++ osx/ArmLinux.sh
@@ -6,9 +6,11 @@ export MASON_PLATFORM="ArmLinux"
 export HOST_PLATFORM="Linux"
 export BOOST_ARCH="arm"
 export ARCH_NAME="armv7"
-source $(dirname "$BASH_SOURCE")/settings.sh
+export CXX="g++"
+export CC="gcc"
 export MAKE="make"
 # default to C++11 for this platform
 if [[ "${CXX11:-false}" == false ]]; then
   export CXX11=true
 fi
+source $(dirname "$BASH_SOURCE")/settings.sh
