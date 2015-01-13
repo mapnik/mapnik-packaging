dd2dfbe7d88e862ef1746202665d7756c3a2d411
Brandon Anderson <bander9289+pub@gmail.com>
2015-01-13 15:49:10 -0600

    Debug environment

--- wrapper.sh
+++ wrapper.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+echo "Initial environment:"
+env
+
 echo "Sourcing build.sh for build functions ..."
 source ./build.sh
+
+echo "Post-build.sh environment:"
+env
 
 echo "Running '${TARGET}' as build target ..."
 ${TARGET}
