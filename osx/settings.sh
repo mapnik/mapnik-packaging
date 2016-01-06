039aa0d62ffacfc1b2f5a0272317548325301cd8
Dane Springmeyer <dane@mapbox.com>
2016-01-06 14:30:57 -0800

    upgrade webp and sqlite

--- osx/settings.sh
+++ osx/settings.sh
@@ -420,7 +420,7 @@ export ICU_VERSION2="56_1"
 export BOOST_VERSION="1.59.0"
 export BOOST_VERSION2="1_59_0"
 # http://www.sqlite.org/download.html
-export SQLITE_VERSION="3090200"
+export SQLITE_VERSION="3100000"
 # http://download.savannah.gnu.org/releases/freetype/freetype-2.5.3.tar.bz2
 # http://nongnu.askapache.com/freetype/freetype-2.5.5.tar.bz2
 export FREETYPE_VERSION="2.6.1"
@@ -434,7 +434,7 @@ export LIBPNG_VERSION="1.6.20"
 export LIBTIFF_VERSION="4.0.6"
 # https://developers.google.com/speed/webp/download
 # http://downloads.webmproject.org/releases/webp/index.html
-export WEBP_VERSION="0.4.4"
+export WEBP_VERSION="0.5.0"
 # http://download.osgeo.org/geotiff/libgeotiff/
 export LIBGEOTIFF_VERSION="1.4.1"
 export JPEG_VERSION="8d"
